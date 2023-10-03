dee365e549601080f5dc0bca66c942e9dcb6e207
Mary <marinakhripankova@gmail.com>
2023-10-04 00:21:56 +0300

    pr14 v1.9

--- app.js
+++ app.js
@@ -1,69 +1,3 @@
-/*
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { errors, celebrate, Joi } = require('celebrate');
-const auth = require('./middlewares/auth');
-const { createUser, login } = require('./controllers/users');
-const NotFoundError = require('./errors/NotFoundError');
-
-const {
-  PORT = 3000,
-  MONGO_URL = 'mongodb://localhost:27017/mestodb',
-} = process.env;
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect(MONGO_URL);
-
-app.use(cors());
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/^(https?:\/\/)?([\da-z.-]+).([a-z.]{2,6})([/\w.-]*)*\/?$/),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.use(auth);
-
-app.use('/', require('./routes/cards'));
-app.use('/', require('./routes/users'));
-
-app.use(errors());
-
-app.use('/*', () => {
-  throw new NotFoundError('Страница не найдена');
-});
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка!'
-        : message,
-    });
-  next();
-});
-
-app.listen(PORT);
-*/
 const express = require('express');
 
 const mongoose = require('mongoose');
@@ -94,6 +28,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required(),
   }),
 }), createUser);
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
